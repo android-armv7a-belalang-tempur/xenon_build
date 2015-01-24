980194fc1e88381eb81b68d756cdb2c6803ce605
Paul Beeler <pbeeler80@gmail.com>
2015-01-24 15:41:02 -0700

    un-LIQUIFY:
    These changes are from sabermod so update with copyright in years.
    
    Added -O3 modular optimizations.  To switch on use O3_OPTIMIZATIONS := true
    Google host gcc 4.8 is now default.  seems silly to have 4.6 as default.  To use Google
    host 4.6, set USE_LEGACY_GCC := true
    Add the updated version of graphite that works and set it on by default (top sabermod feature)
    
    Signed-off-by: Paul Beeler <pbeeler80@gmail.com>
    
    Conflicts:
    	core/binary.mk
    	core/clang/HOST_x86_common.mk
    	core/clang/config.mk
    	core/combo/HOST_linux-x86.mk
    	core/combo/HOST_linux-x86_64.mk
    	core/combo/TARGET_linux-arm.mk
    	core/combo/select.mk
    	core/graphite.mk

--- core/graphite.mk
+++ core/graphite.mk
@@ -1,4 +1,4 @@
-# Copyright (C) 2014 The SaberMod Project
+# Copyright (C) 2014-2015 The SaberMod Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -12,63 +12,48 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
+GRAPHITE_FLAGS := \
+	-fgraphite \
+	-fgraphite-identity \
+	-floop-flatten \
+	-floop-parallelize-all \
+	-ftree-loop-linear \
+	-floop-interchange \
+	-floop-strip-mine \
+	-floop-block \
+	-Wno-error=maybe-uninitialized
 
-GRAPHITE_FLAGS := $(call cc-option,$(-fgraphite -floop-flatten -floop-parallelize-all -ftree-loop-linear -floop-interchange -floop-strip-mine -floop-block))
+# Force disable some modules that are not compatible with graphite flags
+LOCAL_DISABLE_GRAPHITE := \
+	libunwind \
+	libFFTEm \
+	libicui18n \
+	libskia \
+	libvpx \
+	libmedia_jni \
+	libstagefright_mp3dec \
+	libart \
+	mdnsd \
+	libwebrtc_spl \
+	third_party_WebKit_Source_core_webcore_svg_gyp
 
-# Only use graphite if using SaberMod toolchain.  Allow any version of SaberMod with the wildcard symbol.
-ifneq ($(filter (SaberMod%), $(shell $(TARGET_CC) --version)),)
-ifdef LOCAL_CFLAGS
-LOCAL_CFLAGS += $(GRAPHITE_FLAGS)
-else
-LOCAL_CFLAGS := $(GRAPHITE_FLAGS)
-endif
+ifneq (1,$(words $(filter $(LOCAL_DISABLE_GRAPHITE),$(LOCAL_MODULE))))
+  ifdef LOCAL_CONLYFLAGS
+    LOCAL_CONLYFLAGS += $(GRAPHITE_FLAGS)
+  else
+    LOCAL_CONLYFLAGS := $(GRAPHITE_FLAGS)
+  endif
 
-ifdef LOCAL_CPPFLAGS
-LOCAL_CPPFLAGS += $(GRAPHITE_FLAGS)
-else
-LOCAL_CPPFLAGS := $(GRAPHITE_FLAGS)
-endif
+  ifdef LOCAL_CPPFLAGS
+    LOCAL_CPPFLAGS += $(GRAPHITE_FLAGS)
+  else
+    LOCAL_CPPFLAGS := $(GRAPHITE_FLAGS)
+  endif
 
-ifdef $(combo_2nd_arch_prefix)TARGET_arm_CFLAGS
-$(combo_2nd_arch_prefix)TARGET_arm_CFLAGS += $(GRAPHITE_FLAGS)
-else
-$(combo_2nd_arch_prefix)TARGET_arm_CFLAGS := $(GRAPHITE_FLAGS)
-endif
-
-ifdef $(combo_2nd_arch_prefix)TARGET_thumb_CFLAGS
-$(combo_2nd_arch_prefix)TARGET_thumb_CFLAGS += $(GRAPHITE_FLAGS)
-else
-$(combo_2nd_arch_prefix)TARGET_thumb_CFLAGS := $(GRAPHITE_FLAGS)
-endif
-
-ifdef $(combo_2nd_arch_prefix)TARGET_GLOBAL_CPPFLAGS
-$(combo_2nd_arch_prefix)TARGET_GLOBAL_CPPFLAGS += $(GRAPHITE_FLAGS)
-else
-$(combo_2nd_arch_prefix)TARGET_GLOBAL_CPPFLAGS := $(GRAPHITE_FLAGS)
-endif
-
-ifdef $(combo_2nd_arch_prefix)TARGET_RELEASE_CPPFLAGS
-$(combo_2nd_arch_prefix)TARGET_RELEASE_CPPFLAGS += $(GRAPHITE_FLAGS)
-else
-$(combo_2nd_arch_prefix)TARGET_RELEASE_CPPFLAGS := $(GRAPHITE_FLAGS)
-endif
-
-ifdef $(combo_var_prefix)GLOBAL_CFLAGS
-$(combo_var_prefix)GLOBAL_CFLAGS += $(GRAPHITE_FLAGS)
-else
-$(combo_var_prefix)GLOBAL_CFLAGS := $(GRAPHITE_FLAGS)
-endif
-
-ifdef $(combo_var_prefix)RELEASE_CFLAGS
-$(combo_var_prefix)RELEASE_CFLAGS += $(GRAPHITE_FLAGS)
-else
-$(combo_var_prefix)RELEASE_CFLAGS := $(GRAPHITE_FLAGS)
-endif 
-
-ifdef $(combo_var_prefix)GLOBAL_CPPFLAGS
-$(combo_var_prefix)GLOBAL_CPPFLAGS += $(GRAPHITE_FLAGS)
-else
-$(combo_var_prefix)GLOBAL_CPPFLAGS := $(GRAPHITE_FLAGS)
-endif
+  ifndef LOCAL_LDFLAGS
+    LOCAL_LDFLAGS  := $(GRAPHITE_FLAGS)
+  else
+    LOCAL_LDFLAGS  += $(GRAPHITE_FLAGS)
+  endif
 endif
 #####
