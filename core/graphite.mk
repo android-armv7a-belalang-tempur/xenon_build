690000ab476d3eae9bffac3f4f432d7b1c28fcd0
frap129 <frap129@gmail.com>
2015-03-13 19:32:36 -0400

    Add Anti-Arham Protection
    Signed-off-by: frap129 <frap129@gmail.com>
    
    Conflicts:
    	core/graphite.mk

--- core/graphite.mk
+++ core/graphite.mk
@@ -12,6 +12,44 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
+# Find OS
+  UNAME := $(shell uname -s)
+ifeq ($(strip $(UNAME)),Linux)
+    # Graphite flags and friends
+    GRAPHITE_FLAGS := \
+      -fgraphite \
+      -fgraphite-identity \
+      -floop-flatten \
+      -floop-parallelize-all \
+      -ftree-loop-linear \
+      -floop-interchange \
+      -floop-strip-mine \
+      -floop-block
+
+    # Legacy gcc doesn't understand this flag
+    ifneq ($(strip $(USE_LEGACY_GCC)),true)
+      GRAPHITE_FLAGS += \
+        -Wno-error=maybe-uninitialized
+    endif
+
+    # Force disable some modules that are not compatible with graphite flags.
+    # Add more modules if needed for devices in BoardConfig.mk
+    # LOCAL_DISABLE_GRAPHITE +=
+    LOCAL_DISABLE_GRAPHITE := \
+      libunwind \
+      libFFTEm \
+      libicui18n \
+      libskia \
+      libvpx \
+      libmedia_jni \
+      libstagefright_mp3dec \
+      mdnsd \
+      libwebrtc_spl \
+      third_party_WebKit_Source_core_webcore_svg_gyp \
+      libjni_filtershow_filters \
+      libavformat \
+      libavcodec \
+      skia_skia_library_gyp
 
 ifneq (1,$(words $(filter $(LOCAL_DISABLE_GRAPHITE),$(LOCAL_MODULE))))
   ifdef LOCAL_CONLYFLAGS
@@ -31,5 +69,6 @@ ifneq (1,$(words $(filter $(LOCAL_DISABLE_GRAPHITE),$(LOCAL_MODULE))))
   else
     LOCAL_LDFLAGS  += $(GRAPHITE_FLAGS)
   endif
+endif
 endif
 #####
