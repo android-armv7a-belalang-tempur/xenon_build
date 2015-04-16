23f534bac703d1b5af5602eac4fb171f17aa6abe
Paul Beeler <pbeeler80@gmail.com>
2015-04-16 00:44:38 -0600

    Add some basic host optimizations (2/2)
    This is mostly beneficial when used with -O3 optimizations
    
    Change-Id: I53bab4e9edba04046a6e5f726749bba483ed92a9
    Signed-off-by: Paul Beeler <pbeeler80@gmail.com>
    
    Conflicts:
    	core/binary.mk

--- core/binary.mk
+++ core/binary.mk
@@ -113,35 +113,12 @@ ifeq ($(strip $(ELECTRIFY)),true)
 # See the License for the specific language governing permissions and
 # limitations under the License.
 ##########################################################################
-<<<<<<< HEAD
   # Add pthread support
   ifneq ($(strip $(ENABLE_PTHREAD)),false)
     include $(BUILD_SYSTEM)/pthread.mk
 
-# Extra SaberMod C flags for gcc and clang
-# These are most likely arch specific, so do not bother including the host compiler.
-ifdef EXTRA_SABERMOD_GCC_CFLAGS
-  ifneq ($(strip $(LOCAL_IS_HOST_MODULE)),true)
-    ifneq ($(strip $(LOCAL_CLANG)),true)
-      ifdef LOCAL_CFLAGS
-        LOCAL_CFLAGS += $(EXTRA_SABERMOD_GCC_CFLAGS)
-      else
-        LOCAL_CFLAGS := $(EXTRA_SABERMOD_GCC_CFLAGS)
-      endif
-    endif
-  endif
-endif
-
-ifdef EXTRA_SABERMOD_CLANG_CFLAGS
-  ifneq ($(strip $(LOCAL_IS_HOST_MODULE)),true)
-    ifeq ($(strip $(LOCAL_CLANG)),true)
-      ifdef LOCAL_CFLAGS
-        LOCAL_CFLAGS += $(EXTRA_SABERMOD_CLANG_CFLAGS)
-      else
-        LOCAL_CFLAGS := $(EXTRA_SABERMOD_CLANG_CFLAGS)
-      endif
-    endif
-  endif
+  # Extra sabermod variables
+  include $(BUILD_SYSTEM)/extra.mk
 
   # Add some CPU tuning flags
   ifneq ($(strip $(TUNE_CPU)),false)
